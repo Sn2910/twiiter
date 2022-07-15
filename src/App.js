8ee91ebd955e82f6998531c99352f06202c0f173
Ling <abbylinglin@gmail.com>
2022-07-15 16:51:14 +0200

    finalize sidebar design

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Newsfeed from './Components/Newsfeed/Newsfeed';
-import Sidebar from './Components/Sidebar/Sidebar';
-
+import logo from './logo.svg'
+import './App.css'
+import Newsfeed from './Components/Newsfeed/Newsfeed'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 function App() {
-  return (
-    <div className="App">
-      <main>
-      <Sidebar />
-      <Newsfeed />
- 
-      </main>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <main>
+                <Sidebar />
+                <Newsfeed />
+            </main>
+        </div>
+    )
 }
 
-export default App;
+export default App
