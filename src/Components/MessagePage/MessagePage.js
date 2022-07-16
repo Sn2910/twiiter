50de7cd747474091da21a4f4009b5e57ed6e2da8
Ling <abbylinglin@gmail.com>
2022-07-16 16:45:19 +0200

    finish MessagePage

--- src/Components/MessagePage/MessagePage.js
+++ src/Components/MessagePage/MessagePage.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import './MessagePage.css'
 import { useParams } from 'react-router-dom'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import { useState, useEffect } from 'react'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
+import RepeatIcon from '@mui/icons-material/Repeat'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import PublishIcon from '@mui/icons-material/Publish'
 
+const url = 'https://apiminitwitter.herokuapp.com/messages'
 function MessagePage() {
     const { id } = useParams()
+    const [message, setMessage] = useState([])
+    useEffect(() => {
+        fetch(`${url}/${id}`)
+            .then((Response) => Response.json())
+            .then((data) => setMessage(data))
+            .catch((err) => console.log(err))
+    }, [])
+    console.log(message)
     return (
         <div>
-            <h1>{id}</h1>
+            <div className="message_header">
+                <ArrowBackIcon /> back to home page
+            </div>
             {/* Dedicated Post */}
+            <div className="message_body">
+                {message.text}
+                <img src={message.post_image} />
+            </div>
+            <div>{message.createdAt}</div>
+            <div className="post-footer">
+                <ChatBubbleOutlineIcon fontSize="small" />
+                <RepeatIcon fontSize="small" />
+                <FavoriteBorderIcon fontSize="small" />
+
+                <PublishIcon fontSize="small" />
+            </div>
         </div>
     )
 }
