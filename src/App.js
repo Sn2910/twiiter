f7a4eaa8026c0624b04c720e955ad63f2cca323f
Ling <abbylinglin@gmail.com>
2022-07-16 10:05:50 +0200

    update sidebar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg'
 import './App.css'
 import Newsfeed from './Components/Newsfeed/Newsfeed'
 import Sidebar from './Components/Sidebar/Sidebar'
+import { useEffect } from 'react'
 
 function App() {
+    useEffect(() => {})
     return (
         <div className="App">
             <main>
---
+++ src/Components/MessagePage/DedicatedMessage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './MessagePage.css'
+function DedicatedMessage() {
+    return <div></div>
+}
+
+export default MessagePage
