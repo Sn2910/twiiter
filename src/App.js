9fe4953ed5f8052b939f290a6d55696511a7a0a4
danilobml <danbarml@gmail.com>
2022-07-20 00:18:31 +0200

    author profilr

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import logo from './logo.svg'
-import './App.css'
-import Newsfeed from './Components/Newsfeed/Newsfeed'
-import Sidebar from './Components/Sidebar/Sidebar'
-import Advertise from './Components/Advertise/Advertise'
-import Post from './Components/Post/Post'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import MessagePage from './Components/MessagePage/MessagePage'
-import Profile from './Components/Profile/Profile'
+import "./App.css";
+import Newsfeed from "./Components/Newsfeed/Newsfeed";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Advertise from "./Components/Advertise/Advertise";
+import { Routes, Route, Navigate } from "react-router-dom";
+import MessagePage from "./Components/MessagePage/MessagePage";
+import Profile from "./Components/Profile/Profile";
+import ProfilePosters from "./Components/ProfilePosters/ProfilePosters";
 
 function App() {
-    return (
-        <div className="app">
-            <main>
-                <Sidebar />
-                    <Routes>
-                        <Route
-                            path="/messagePage/:id"
-                            element={<MessagePage />}
-                        />
-                        <Route path="/" element={<Newsfeed />} />
-                       <Route path ="/profile" element={<Profile />} /> 
-                        
-                    </Routes>
-
-                <Advertise />
-            </main>
-            
-        </div>
-    )
+  return (
+    <div className="app">
+      <main>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Newsfeed />} />
+          <Route path="/messagePage/:id" element={<MessagePage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:id" element={<ProfilePosters />} />
+          <Route path="/twitter" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Advertise />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,53 +1,54 @@
-import React from 'react'
-import './Sidebar.css'
-import SidebarOption from './SidebarOption'
-import SignLanguageSharpIcon from '@mui/icons-material/SignLanguageSharp'
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
-import SearchIcon from '@mui/icons-material/Search'
-import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined'
-import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined'
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined'
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
-import Button from '@mui/material/Button'
-import { NavLink, useNavigate }  from 'react-router-dom'
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import SignLanguageSharpIcon from "@mui/icons-material/SignLanguageSharp";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import MailOutlineOutlinedIcon from "@mui/icons-material/MailOutlineOutlined";
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 function Sidebar() {
-    const navigate = useNavigate()
-    return (
-        <div className="sidebar">
-            {/* Icon */}
-            <center>
-                <SignLanguageSharpIcon className="sidebar_logo" />
-            </center>
+  const navigate = useNavigate();
+  return (
+    <div className="sidebar">
+      {/* Icon */}
+      <center>
+        <SignLanguageSharpIcon className="sidebar_logo" />
+      </center>
 
-            {/* SidebarOption*/}
-            <SidebarOption active Icon={HomeOutlinedIcon} text="Home" />
-            <SidebarOption Icon={SearchIcon} text="Explore" />
-            <SidebarOption
-                Icon={NotificationsNoneOutlinedIcon}
-                text="Notifications"
-            />
-            <SidebarOption Icon={MailOutlineOutlinedIcon} text="Messages" />
+      {/* SidebarOption*/}
+      <div
+        className="home"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <SidebarOption active Icon={HomeOutlinedIcon} text="Home" />
+      </div>
+      <SidebarOption Icon={SearchIcon} text="Explore" />
+      <SidebarOption Icon={NotificationsNoneOutlinedIcon} text="Notifications" />
+      <SidebarOption Icon={MailOutlineOutlinedIcon} text="Messages" />
 
-           <div className="prof"  onClick={()=>{navigate("/profile")}}>
-           <SidebarOption Icon={PermIdentityOutlinedIcon} text="Profile" />
-           </div>
-          
-           
-           
-           
-            
-            <SidebarOption Icon={MoreHorizIcon} text="More" />
+      <div
+        className="prof"
+        onClick={() => {
+          navigate("/profile");
+        }}
+      >
+        <SidebarOption Icon={PermIdentityOutlinedIcon} text="Profile" />
+      </div>
 
-            
-           
-            
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-            {/* Button--Clap */}
-            <Button variant="outlined" className="sidebar_tweet" fullWidth>
-                Clap
-            </Button>
-        </div>
-    )
+      {/* Button--Clap */}
+      <Button variant="outlined" className="sidebar_tweet" fullWidth>
+        Clap
+      </Button>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
